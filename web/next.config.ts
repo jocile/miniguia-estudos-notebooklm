6c71d0dfcc67b7c9776a75221857be0e39ab2ffb
jocile <jocile@gmail.com>
2026-04-07 18:49:08 -0300

    fix: next.config.ts basePath para funcionar corretamente no github pages

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isGithubActions ? "/miniguia-estudos-notebooklm" : "",
   images: {
     unoptimized: true,
   },
